694301c9611606e1020a53041ef088d4d7f3ff69
Kristijan Prematarov <kristijanprematarov@yahoo.com>
2022-11-09 16:04:44 +0100

    Add project files.

---
+++ TypeView/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using System.Text;
+
+StringBuilder OutputText = new();
+
+// modify this line to retrieve details of any other data type
+Type t = typeof(double);
+
+AnalyzeType(t);
+Console.WriteLine($"Analysis of type {t.Name}");
+Console.WriteLine(OutputText.ToString());
+
+Console.ReadLine();
+
+void AnalyzeType(Type t)
+{
+    TypeInfo typeInfo = t.GetTypeInfo();
+    AddToOutput($"Type Name: {t.Name}");
+    AddToOutput($"Full Name: {t.FullName}");
+    AddToOutput($"Namespace: {t.Namespace}");
+
+    Type? tBase = typeInfo.BaseType;
+
+    if (tBase != null)
+    {
+        AddToOutput($"Base Type: {tBase.Name}");
+    }
+
+    ShowMembers("constructors", t.GetConstructors());
+    ShowMembers("methods", t.GetMethods());
+    ShowMembers("properties", t.GetProperties());
+    ShowMembers("fields", t.GetFields());
+    ShowMembers("events", t.GetEvents());
+
+    void ShowMembers(string title, IList<MemberInfo> members)
+    {
+        if (members.Count == 0) return;
+        AddToOutput($"\npublic {title}:");
+        var names = members.Select(m => m.Name).Distinct();
+        AddToOutput(string.Join(" ", names));
+    }
+
+    void AddToOutput(string Text) =>
+        OutputText.Append($"{Text}{Environment.NewLine}");
+}
---
+++ WhatsNewAttributes/WhatsNewAttributes.cs
@@ -0,0 +1,25 @@
+﻿namespace WhatsNewAttributes;
+
+[AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Constructor, AllowMultiple = true, Inherited = false)]
+public class LastModifiedAttribute : Attribute
+{
+    private readonly DateTime _dateModified;
+    private readonly string _changes;
+
+    public LastModifiedAttribute(string dateModified, string changes)
+    {
+        _dateModified = DateTime.Parse(dateModified);
+        _changes = changes;
+    }
+
+    public DateTime DateModified => _dateModified;
+
+    public string Changes => _changes;
+
+    public string? Issues { get; set; }
+}
+
+[AttributeUsage(AttributeTargets.Assembly)]
+public class SupportsWhatsNewAttribute : Attribute
+{
+}
